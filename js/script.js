47f09caaebf51e1740dcbfa81d9acc0166fc4132
photographyMF <homemcardoso@gmail.com>
2025-04-21 13:18:55 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+// Adiciona funcionalidades interativas para a galeria
+document.addEventListener('DOMContentLoaded', () => {
+    const gallery = document.querySelector('.gallery');
+    const images = [
+        'images/paisagem1.jpg',
+        'images/macro1.jpg',
+        'images/detalhes1.jpg',
+        'images/viagem1.jpg'
+    ];
 
+    images.forEach(src => {
+        const img = document.createElement('img');
+        img.src = src;
+        img.alt = 'Fotografia';
+        gallery.appendChild(img);
+    });
+});
